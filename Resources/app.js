79006f72e9b73893fd69100d5d8e17708b37fbff
Eric Merriman <eric34@gmail.com>
2014-05-05 23:09:50 -0700

    Added some ui, and score indicators. Started making card display function. Fixed views for player and dealer.

--- Resources/app.js
+++ Resources/app.js
@@ -11,17 +11,59 @@ var heartImage = Ti.Filesystem.getFile(Titanium.Filesystem.resourcesDirectory, '
 var diamondImage = Ti.Filesystem.getFile(Titanium.Filesystem.resourcesDirectory, '/images/diamond.png');
 var spadeImage = Ti.Filesystem.getFile(Titanium.Filesystem.resourcesDirectory, '/images/spade.png');
 
-// Make a couple views to seperate the card dealing area
-// Right now I have these screwed up and the player cards go to the dealer area
-var dealerView = Ti.UI.createView({
-	layout : 'horizontal'
+// Make some buttons for the UI
+var hitButton = Ti.UI.createButton({
+	height : 40,
+	width : 100,
+	bottom : 0,
+	title : 'hit'
 });
 
-var playerView = Ti.UI.createView({
+var standButton = Ti.UI.createButton({
+	height : 40,
+	width : 100,
+	bottom : 0,
+	title : 'stand'
+});
+
+hitButton.addEventListener('click', function() {
+	hit();
+});
+
+standButton.addEventListener('click', function() {
+	stand();
+});
+
+// Make a couple views to seperate the card dealing area
+var dealerView = Ti.UI.createView({
 	layout : 'horizontal',
 	backgroundColor : 'green',
 	height : '50%'
 });
+
+var dealerScore = Ti.UI.createLabel({
+	text : '0',
+	left : 5,
+	height : 36,
+	textAlign : 'center'
+});
+
+dealerView.add(dealerScore);
+
+var playerView = Ti.UI.createView({
+	layout : 'horizontal',
+	backgroundColor : 'yellow',
+	height : '50%'
+});
+
+var playerScore = Ti.UI.createLabel({
+	text : '0',
+	left : 5,
+	height : 36,
+	textAlign : 'center'
+});
+
+playerView.add(playerScore);
 
 var firstCard = Ti.UI.createView({
 	layout : 'vertical',
@@ -71,17 +113,16 @@ var pCard2 = Ti.UI.createImageView({
 	image : heartImage
 });
 
+
 firstCard.add(label1);
 firstCard.add(pCard1);
 
 secondCard.add(label2);
 secondCard.add(pCard2);
 
-playerView.add(firstCard);
-playerView.add(secondCard);
+dealerView.add(firstCard);
+dealerView.add(secondCard);
 
-// win.add(dealerView);
-// win.add(playerView);
 
 dealer_hand = new Array();
 player_hand = new Array();
@@ -135,7 +176,7 @@ function shuffle() {
 // This deals the next card off the deck
 function dealCard() {
 	Ti.API.info("dealing");
-	
+
 	return this.cards[this.next_card++];
 }
 
@@ -143,6 +184,13 @@ function dealCard() {
 var deck = new Deck();
 deck.shuffle();
 
+// The display function
+function displayCard(player, card, up) {
+	if (player === "player") {
+
+	}
+
+}
 
 // The main loop, slowly converting from a javascript web page
 function newGame() {
@@ -153,18 +201,35 @@ function newGame() {
 		deck.shuffle();
 		// the cards have been used.
 	}
+
+	// make an array for each hand
 	dealer_hand = new Array();
 	player_hand = new Array();
 
-	// Deal and Display cards - the old way
+	// player gets one up first, then dealer down, then player up, then dealer up
+	player_hand[0] = deck.dealCard();
+	displayCard('player', player_hand[0], true);
 
 	dealer_hand[0] = deck.dealCard();
 	// This is the hole card.
-	dealer_hand[1] = deck.dealCard();
+	displayCard('dealer', dealer_hand[0], false);
 
+	player_hand[1] = deck.dealCard();
+	displayCard('player', player_hand[1], true);
+
+	dealer_hand[1] = deck.dealCard();
+	displayCard('dealer', dealer_hand[1], true);
+	
+	//add up the score
+	playerScore.text = score(player_hand);
+	dealerScore.text = score(dealer_hand);
+
+	//Some logging to check my display
 	Ti.API.info("hit the first deal");
-	Ti.API.info("It's a " + dealer_hand[0].num + " " + dealer_hand[0].suit);
-	Ti.API.info("It's a " + dealer_hand[1].num + " " + dealer_hand[1].suit);
+	Ti.API.info("Dealer first card is a " + dealer_hand[0].num + " " + dealer_hand[0].suit);
+	Ti.API.info("Dealer Second is a " + dealer_hand[1].num + " " + dealer_hand[1].suit);
+	Ti.API.info("Player first card is a " + player_hand[0].num + " " + player_hand[0].suit);
+	Ti.API.info("Player Second is a " + player_hand[1].num + " " + player_hand[1].suit);
 
 	label1.text = dealer_hand[0].num;
 	label2.text = dealer_hand[1].num;
@@ -191,23 +256,9 @@ function newGame() {
 		pCard2.image = clubImage;
 	}
 
-	win.add(playerView);
-
-	var button1 = Ti.UI.createButton({
-		height : 40,
-		width : 100,
-		title : 'Shuffle'
-	});
-
-	button1.addEventListener('click', function() {
-		dealer_hand = [];
-		deck.shuffle();
-		newGame();
-
-	});
-
-	dealerView.add(button1);
 	win.add(dealerView);
+	win.add(playerView);
+	
 	// document.images[0].src = "blank.gif"; // The hole card is not shown
 	// dealer_hand[ 1 ] = deck.dealCard();
 	// document.images[ 1 ].src = dealer_hand[ 1 ].fname();
@@ -224,9 +275,6 @@ function newGame() {
 	// }
 	// //end the old way
 
-	// Deal and display the new way
-
-	//end the new way
 
 	// // Reset the form fields and the state variables
 	// window.status = "";
@@ -242,21 +290,23 @@ function hit() {
 	var new_card = 0;
 	// index for the new card position
 	if (game_over) {
-		//window.status = "Game over.  Click the Deal button to start a new hand."
+		alert("Game over.  Click the Deal button to start a new hand.");
 	} else {
 		new_card = player_hand.length;
 		player_hand[new_card] = deck.dealCard();
-		document.images[new_card + 6].src = player_hand[new_card].fname();
+		//document.images[new_card + 6].src = player_hand[new_card].fname();
 		total = score(player_hand);
+		playerScore.text = total;
+		
 		if (total > 21) {// Busted, game over.
-			document.form1.player.value = total + "  busted";
-			document.images[0].src = dealer_hand[0].fname();
+			alert("  busted");
+			//document.images[0].src = dealer_hand[0].fname();
 			// reveal the dealer hole card
-			document.form1.dealer.value = score(dealer_hand);
+			//document.form1.dealer.value = score(dealer_hand);
 			winner();
 			game_over = true;
 		} else {
-			document.form1.player.value = total;
+			//document.form1.player.value = total;
 		}
 	}
 }// end function hit()
@@ -271,6 +321,8 @@ function stand() {
 
 		// document.images[ 0 ].src = dealer_hand[ 0 ].fname(); // reveal the dealer hole card
 		while (score(dealer_hand) < 17) {// Dealer stands on soft 17
+			dealerScore.text = score(dealer_hand);
+			
 			new_card = dealer_hand.length;
 			dealer_hand[new_card] = deck.dealCard();
 			//  document.images[ new_card ].src = dealer_hand[ new_card ].fname();
@@ -278,9 +330,10 @@ function stand() {
 
 		total = score(dealer_hand);
 		if (total > 21) {// Busted
-			//  document.form1.dealer.value = total + "  busted";
+			dealerScore.text = score(dealer_hand);
+			alert('dealer busted');
 		} else {
-			//  document.form1.dealer.value = total;
+			dealerScore.text = score(dealer_hand);
 		}
 
 	}
@@ -338,6 +391,32 @@ function winner() {
 		}
 	}
 }
+
+// Make some buttons for the UI
+var hitButton = Ti.UI.createButton({
+	height : 40,
+	width : 100,
+	bottom : 0,
+	title : 'hit'
+});
+
+var standButton = Ti.UI.createButton({
+	height : 40,
+	width : 100,
+	bottom : 0,
+	title : 'stand'
+});
+
+hitButton.addEventListener('click', function() {
+	hit();
+});
+
+standButton.addEventListener('click', function() {
+	stand();
+});
+
+playerView.add(hitButton);
+playerView.add(standButton);
 
 win.open();
 newGame();
